d2792c9b8ca689915d31b930d2c14a5696fed66f
rockgarden <ro>
2017-12-26 15:51:22 +0800

    init

---
+++ NeuralNetworks/NeuralNetworks/NeuralNetworks.h
@@ -0,0 +1,19 @@
+//
+//  NeuralNetworks.h
+//  NeuralNetworks
+//
+//  Created by wangkan on 2017/12/26.
+//  Copyright © 2017年 wangkan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for NeuralNetworks.
+FOUNDATION_EXPORT double NeuralNetworksVersionNumber;
+
+//! Project version string for NeuralNetworks.
+FOUNDATION_EXPORT const unsigned char NeuralNetworksVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NeuralNetworks/PublicHeader.h>
+
+
