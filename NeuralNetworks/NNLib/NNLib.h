896e9c65eee95dabe20330ed1d63071e7a74342d
rockgarden <ro>
2017-12-27 14:03:37 +0800

    add

---
+++ NeuralNetworks/NNLib/NNLib.h
@@ -0,0 +1,19 @@
+//
+//  NNLib.h
+//  NNLib
+//
+//  Created by wangkan on 2017/12/27.
+//  Copyright © 2017年 wangkan. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for NNLib.
+FOUNDATION_EXPORT double NNLibVersionNumber;
+
+//! Project version string for NNLib.
+FOUNDATION_EXPORT const unsigned char NNLibVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NNLib/PublicHeader.h>
+
+
